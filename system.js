fd1bf043b85f9bdddf8fdbd9a07124a100833ac6
Shiigehiro <shiigehiro@gmail.com>
2018-01-20 18:28:40 +0200

    Remove connect schema

--- system.js
+++ system.js
@@ -1,7 +1,5 @@
 console.log('\033c');
 
-var colog = require('colog');
-require('fs').readFile(`${process.cwd()}/Core/connect.schema`, 'utf8', (err, data) => {console.log(data)})
 const Discord = require("discord.js");
 const sharder = new Discord.ShardingManager(`${__dirname}/system-client.js`, {totalShards:3,respawn:true});
 
